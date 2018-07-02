ee39a6a0cc11f0abc4b82d50b00d597fa6009e96
Davi Silva <daviribeirosoares@gmail.com>
2018-07-02 01:57:41 -0300

    Adjusting where clause

--- src/UseCases/subscriber/GetSubscribers.js
+++ src/UseCases/subscriber/GetSubscribers.js
@@ -33,7 +33,7 @@ export default {
     },
     resolve(root, args, context){
         if(context.user && (context.user.id === args.userId || context.user.isAdmin)) {
-            return Db.models.subscriber.findAll({where: {'isSubscribed': 1}});
+            return Db.models.subscriber.findAll({where: {'users.isSubscribed': 1}, include: ['users']});
         }
         throw new Error("Não autorizado.");
     }
